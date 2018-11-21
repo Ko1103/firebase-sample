6c23234477ceea034f9a87e7157b18e37c2230da
KO-YAMAKO <ko.foot1103@gmail.com>
2018-11-21 22:03:28 +0900

    add post user

--- functions/index.js
+++ functions/index.js
@@ -36,7 +36,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err: {};
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 // module.exports = app;
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -1,10 +1,46 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+var sys = require('util');
 var express = require('express');
 var router = express.Router();
 
-/* Get user list */
+// setup firestore
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
 
+/* Get user list */
 router.get('/', function(req, res, next) {
-  res.send('response with source');
+  var cityRef = db.collection('cities').doc(req.body.name);
+  var getDoc = cityRef.get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          console.log('Document data:', doc.data());
+          res.json(doc.data());
+        }
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
 });
 
+router.post('/', function(req, res, next) {
+  var newData = req.body
+  // var data = {
+  //   name: 'Los Angeles',
+  //   state: 'CA',
+  //   country: 'USA',
+  //   capital: true
+  // };
+  var docRef = db.collection('cities').doc(newData.name);
+  var setLA = docRef.set(newData).then(ref => {
+    console.log('Added document with ID: ', ref.id);
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+})
 module.exports = router;
